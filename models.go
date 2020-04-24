f1b248010dbc78b25399e4b53e5325e86646817d
Kevin Bintzik <kevin.bintzik@sunzinet.com>
2020-04-24 08:39:46 +0200

    remove pointer

--- models.go
+++ models.go
@@ -25,62 +25,62 @@ type User struct {
 
 // CredentialRepresentation represents credentials
 type CredentialRepresentation struct {
-	Algorithm         *string             `json:"algorithm,omitempty"`
+	Algorithm         string              `json:"algorithm,omitempty"`
 	Config            *MultiValuedHashMap `json:"config,omitempty"`
-	Counter           *int32              `json:"counter,omitempty"`
-	CreatedDate       *int64              `json:"createdDate,omitempty"`
-	Device            *string             `json:"device,omitempty"`
-	Digits            *int32              `json:"digits,omitempty"`
-	HashIterations    *int32              `json:"hashIterations,omitempty"`
-	HashedSaltedValue *string             `json:"hashedSaltedValue,omitempty"`
-	Period            *int32              `json:"period,omitempty"`
-	Salt              *string             `json:"salt,omitempty"`
-	Temporary         *bool               `json:"temporary"`
-	Type              *string             `json:"type,omitempty"`
-	Value             *string             `json:"value,omitempty"`
+	Counter           int32               `json:"counter,omitempty"`
+	CreatedDate       int64               `json:"createdDate,omitempty"`
+	Device            string              `json:"device,omitempty"`
+	Digits            int32               `json:"digits,omitempty"`
+	HashIterations    int32               `json:"hashIterations,omitempty"`
+	HashedSaltedValue string              `json:"hashedSaltedValue,omitempty"`
+	Period            int32               `json:"period,omitempty"`
+	Salt              string              `json:"salt,omitempty"`
+	Temporary         bool                `json:"temporary"`
+	Type              string              `json:"type,omitempty"`
+	Value             string              `json:"value,omitempty"`
 }
 
 // MultiValuedHashMap represents something
 type MultiValuedHashMap struct {
-	Empty      *bool    `json:"empty"`
-	LoadFactor *float32 `json:"loadFactor,omitempty"`
-	Threshold  *int32   `json:"threshold,omitempty"`
+	Empty      bool    `json:"empty"`
+	LoadFactor float32 `json:"loadFactor,omitempty"`
+	Threshold  int32   `json:"threshold,omitempty"`
 }
 
 // GetUsersParams represents the optional parameters for getting users
 type GetUsersParams struct {
-	BriefRepresentation *bool   `json:"briefRepresentation,string"`
-	Email               *string `json:"email,omitempty"`
-	First               *int    `json:"first,string,omitempty"`
-	FirstName           *string `json:"firstName,omitempty"`
-	LastName            *string `json:"lastName,omitempty"`
-	Max                 *int    `json:"max,string,omitempty"`
-	Search              *string `json:"search,omitempty"`
-	Username            *string `json:"username,omitempty"`
+	BriefRepresentation bool   `json:"briefRepresentation,string"`
+	Email               string `json:"email,omitempty"`
+	First               int    `json:"first,string,omitempty"`
+	FirstName           string `json:"firstName,omitempty"`
+	LastName            string `json:"lastName,omitempty"`
+	Max                 int    `json:"max,string,omitempty"`
+	Search              string `json:"search,omitempty"`
+	Username            string `json:"username,omitempty"`
 }
 
 // UserGroup is a UserGroup
 type UserGroup struct {
-	ID   *string `json:"id,omitempty"`
-	Name *string `json:"name,omitempty"`
-	Path *string `json:"path,omitempty"`
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+	Path string `json:"path,omitempty"`
 }
 
 // GetUsersByRoleParams represents the optional parameters for getting users by role
 type GetUsersByRoleParams struct {
-	First *int `json:"first,string,omitempty"`
-	Max   *int `json:"max,string,omitempty"`
+	First int `json:"first,string,omitempty"`
+	Max   int `json:"max,string,omitempty"`
 }
 
 // UserSessionRepresentation represents a list of user's sessions
 type UserSessionRepresentation struct {
 	Clients    map[string]string `json:"clients,omitempty"`
-	ID         *string           `json:"id,omitempty"`
-	IPAddress  *string           `json:"ipAddress,omitempty"`
-	LastAccess *int64            `json:"lastAccess,omitempty"`
-	Start      *int64            `json:"start,omitempty"`
-	UserID     *string           `json:"userId,omitempty"`
-	Username   *string           `json:"username,omitempty"`
+	ID         string            `json:"id,omitempty"`
+	IPAddress  string            `json:"ipAddress,omitempty"`
+	LastAccess int64             `json:"lastAccess,omitempty"`
+	Start      int64             `json:"start,omitempty"`
+	UserID     string            `json:"userId,omitempty"`
+	Username   string            `json:"username,omitempty"`
 }
 
 // CertResponseKey is returned by the certs endpoint
@@ -109,18 +109,18 @@ type Token struct {
 
 // UserInfo is returned by the userinfo endpoint
 type UserInfo struct {
-	Sub               *string     `json:"sub,omitempty"`
-	EmailVerified     *bool       `json:"email_verified"`
+	Sub               string      `json:"sub,omitempty"`
+	EmailVerified     bool        `json:"email_verified"`
 	Address           interface{} `json:"address,omitempty"`
-	PreferredUsername *string     `json:"preferred_username,omitempty"`
-	Email             *string     `json:"email,omitempty"`
+	PreferredUsername string      `json:"preferred_username,omitempty"`
+	Email             string      `json:"email,omitempty"`
 }
 
 // Group is a Group
 type Group struct {
-	ID          *string             `json:"id,omitempty"`
-	Name        *string             `json:"name,omitempty"`
-	Path        *string             `json:"path,omitempty"`
+	ID          string              `json:"id,omitempty"`
+	Name        string              `json:"name,omitempty"`
+	Path        string              `json:"path,omitempty"`
 	SubGroups   []*Group            `json:"subGroups,omitempty"`
 	Attributes  map[string][]string `json:"attributes,omitempty"`
 	Access      map[string]bool     `json:"access,omitempty"`
